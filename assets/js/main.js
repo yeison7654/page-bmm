fa23a211ce0fb656f7e13ecce4ed626e727d6a8e
yeison7654 <yeisoncarhuapoma@gmail.com>
2024-07-03 10:42:52 -0500

    implementacion de carousel

---
+++ assets/js/main.js
@@ -0,0 +1,58 @@
+let pageLoad = document.querySelector(".load-page");
+/**ejecutar funciones despues de cargar la pagina web **/
+document.addEventListener("DOMContentLoaded", () => {
+    setTimeout(() => {
+        btnMenu()
+        navScroll()
+        carouselMain()
+        /**oculta pantalla de carga */
+        pageLoad.classList.add("hidden")
+    }, 1000);
+})
+/**Funcion de carga de menu en el modo responsivo **/
+function btnMenu() {
+    let btnOpenClose = document.querySelector(".btn-oc");
+    let so = document.querySelector(".section-options");
+    btnOpenClose.addEventListener("click", (e) => {
+        e.preventDefault();
+        if (so.classList.contains("s-hidden")) {
+            so.classList.remove("s-hidden")
+        } else {
+            so.classList.add("s-hidden")
+        }
+    })
+}
+function navScroll() {
+    document.addEventListener("scroll", () => {
+        //capturamos el valor en movimiento del scroll en y
+        scrolly = window.scrollY;
+        if (scrolly > 14
+        ) {
+            document.querySelector(".menu-nav-contact").classList.add("hidden");
+            document.querySelector(".float-up").classList.remove("hidden");
+        } else if (scrolly < 15) {
+            document.querySelector(".menu-nav-contact").classList.remove("hidden");
+            document.querySelector(".float-up").classList.add("hidden");
+        }
+    });
+}
+function carouselMain() {
+    $(".owl-carousel").owlCarousel({
+        loop: true,
+        margin: 10,
+        nav: true,
+        autoplay: true,
+        autoplayTimeout: 4000,
+        responsive: {
+            0: {
+                items: 1
+            },
+            720: {
+                items: 1
+            },
+            1080: {
+                items: 1
+            }
+        }
+    });
+}
